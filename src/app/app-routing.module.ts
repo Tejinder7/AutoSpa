89c8ab10a69095afe12e578f70ce72f1cd4b4540
Tejinder Singh <t.saggu99@gmail.com>
2021-12-12 00:12:25 +0530

    Rough design of Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'auth', component: AuthenticateComponent}
+  {path: 'auth', component: AuthenticateComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticateComponent
+    AuthenticateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
